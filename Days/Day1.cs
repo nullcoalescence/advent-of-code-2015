ffaaae66f6d95364c082b3e783339f344b6982e2
ben <{username}@users.noreply.github.com>
2022-12-15 22:12:50 -0600

    Modify .csproj to copy 'Input' directory to build dir. Also completed day 1.

--- Days/Day1.cs
+++ Days/Day1.cs
@@ -1,16 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace advent_of_code_2015.Days
+﻿namespace advent_of_code_2015.Days
 {
     internal class Day1 : IDay
     {
         public void Run()
         {
-            Console.WriteLine("Hello");
+            var fileContent = File.ReadAllText(@"Input/day_1.txt");
+
+            var contents = fileContent.ToCharArray();
+
+            int floor = 0;
+            int basementIndex = -1;
+
+            for (int i = 0; i < contents.Length; i++)
+            {
+                if (contents[i].Equals('('))
+                {
+                    floor++;
+                }
+                else if (contents[i].Equals(')'))
+                {
+                    floor--;
+                }
+                else
+                {
+                    throw new InvalidDataException();
+                }
+
+                // Part 2: check if we are on basement
+                if (floor == -1 && basementIndex == -1)
+                {
+                    basementIndex = i + 1;
+                }
+
+                
+            }
+
+            Console.WriteLine($"Part 1: End floor: {floor}");
+            Console.WriteLine($"Part 2: Position of character where basement (-1) entered: {basementIndex}");
         }
     }
 }
