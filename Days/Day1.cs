65fc8a3192014a5a114a1cb1d44c9c16ac7457ad
ben <{username}@users.noreply.github.com>
2022-12-15 21:47:56 -0600

    Created basic 'launcher'. Takes an integer as a param, validates. If invalid, or no param passed, prompt user for a day between 1 & 25.

---
+++ Days/Day1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code_2015.Days
+{
+    internal class Day1 : IDay
+    {
+        public void Run()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
